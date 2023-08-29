24b913fd47d249474b22edd5ba66b471e559052f
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-08-29 16:24:12 +0300

    Module #1. Task #1. Build Tools. Gradle solution

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,23 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+
+        if (args == null || args.isEmpty()) {
+            return false;
+        }
+
+        for (String arg : args) {
+            try {
+                double value = Double.parseDouble(arg);
+                if (value <= 0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+
+        try {
+            double value = Double.parseDouble(str);
+            return value > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
     }
 }
